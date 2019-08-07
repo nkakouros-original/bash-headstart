00a399f4d0a28bee8aa741438020ad14a644bce3
Nikolaos Kakouros <nkak@kth.se>
2019-08-07 22:42:09 +0200

    wip

--- headstart-core.bash
+++ headstart-core.bash
@@ -45,6 +45,10 @@ declare -gx _HEADSTART_CMD="${_GO_CMD##*/}"
 declare -gx _HEADSTART_PROJECT_CONFIG="${HEADSTART_PROJECT_CONFIG-data/config/project.conf}"
 declare -gx _HEADSTART_CORE_LOCK="${HEADSTART_CORE_LOCK-data/config/.core.lock}"
 
+. "$_GO_USE_MODULES" 'core'
+
+core_get_installed_version
+
 function headstart() {
   local debug=false
   local verbosity=''
@@ -108,8 +112,7 @@ function headstart() {
 
   if [[ "$print_version" == 'true' ]]; then
     printf "$_HEADSTART_CMD version: "
-    core_get_installed_version
-    echo "$version"
+    eval "echo \$${_HEADSTART_SCRIPT_NAME~~}_VERSION"
     return
   fi
 
