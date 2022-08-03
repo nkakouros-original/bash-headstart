d676107ef5e927ba6a06010197e9aa207b7d8eb1
Nikolaos Kakouros <nkak@kth.se>
2022-08-03 20:26:14 +0000

    fix url

--- commands/core.d/bootstrap.d/ubuntu.sh
+++ commands/core.d/bootstrap.d/ubuntu.sh
@@ -13,7 +13,7 @@ distro_packages+=(
 pip_packages+=(
   # 'python-gilt'
   'git+http://github.com/coala/git-url-parse@1.1'
-  'git+git://github.com/nkakouros-forks/gilt@all'
+  'git+http://github.com/nkakouros-forks/gilt@all'
 )
 
 sudo apt update
